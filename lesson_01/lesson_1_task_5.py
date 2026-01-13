3e4a0b31b4f1b255b8ea3c00438f2302eaad6115
ZojaDD <zojadr.zd@gmail.com>
2026-01-13 14:26:23 +0300

    first commit

---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def num():
+    print(8, end='')
+    print(8, end='')
+    print(0, end='')
+    print(0, end='')
+    print(5, end='')
+    print(5, end='')
+    print(5, end='')
+    print(3, end='')
+    print(5, end='')
+    print(3, end='')
+    print(5, end='')
+
+
+num()
