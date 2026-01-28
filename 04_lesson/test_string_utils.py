cdda86f52ed1a21384b54fc847fa13c25368c890
ZojaDD <zojadr.zd@gmail.com>
2026-01-28 16:26:03 +0300

    Исправлены ошибки

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -5,6 +5,7 @@ from string_utils import StringUtils
 string_utils = StringUtils()
 
 
+@pytest.mark.positive
 @pytest.mark.parametrize("input_str, expected", [
     ("skypro", "Skypro"),
     ("hello world", "Hello world"),
@@ -14,6 +15,7 @@ def test_capitalize_positive(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
 
 
+@pytest.mark.negative
 @pytest.mark.parametrize("input_str, expected", [
     ("123abc", "123abc"),
     ("", ""),
@@ -23,15 +25,17 @@ def test_capitalize_negative(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
 
 
+@pytest.mark.positive
 @pytest.mark.parametrize("input_str, expected", [
     ("   skypro", "skypro"),
-    ("   Skypro", "Skypro"),
+    ("   2 Skypro", "2 Skypro"),
     (" hello world", "hello world"),
 ])
 def test_trim_positive(input_str, expected):
     assert string_utils.trim(input_str) == expected
 
 
+@pytest.mark.negative
 @pytest.mark.parametrize("input_str, expected", [
     ("   123abc", "123abc"),
     ("   ", ""),
@@ -41,37 +45,41 @@ def test_trim_negative(input_str, expected):
     assert string_utils.trim(input_str) == expected
 
 
-@pytest.mark.parametrize("input_str, input_symbol, expected", [
-    ("SkyPro", "S", "True"),
-    ("Skypro", "h", "False"),
-    ("hello world", "w", "True"),
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("SkyPro", "S", True),
+    ("Skypro", "h", False),
+    ("hello world", "w", True),
 ])
-def test_contains_positive(input_str, input_symbol, expected):
-    assert string_utils.contains(input_str) == expected
+def test_contains_positive(input_str, symbol, expected):
+    assert string_utils.contains(input_str, symbol) == expected
 
 
-@pytest.mark.parametrize("input_str, input_symbol, expected", [
-    ("", "f", "False"),
-    ("   ", "k", "False"),
-    ("   .", ".", "True"),
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("", "f", False),
+    ("   ", "k", False),
+    ("   .", ".", True),
 ])
-def test_contains_negative(input_str, input_symbol, expected):
-    assert string_utils.contains(input_str) == expected
+def test_contains_negative(input_str, symbol, expected):
+    assert string_utils.contains(input_str, symbol) == expected
 
 
-@pytest.mark.parametrize("input_string, symbol, expected", [
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected", [
     ("SkyPro", "k", "SyPro"),
     ("SkyPro", "Pro", "Sky"),
     ("hello world", " world", "hello"),
 ])
-def test_delete_symbol_positive(input_string, input_symbol, expected):
-    assert string_utils.delete_symbol(input_string) == expected
+def test_delete_symbol_positive(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
 
 
-@pytest.mark.parametrize("input_string, input_symbol, expected", [
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, symbol, expected", [
     ("SkyPro", "", "SkyPro"),
     ("SkyPro", "v", "SkyPro"),
     ("   ", "a", "   "),
 ])
-def test_delete_symbol_negative(input_string, input_symbol, expected):
-    assert string_utils.delete_symbol(input_string) == expected
+def test_delete_symbol_negative(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
