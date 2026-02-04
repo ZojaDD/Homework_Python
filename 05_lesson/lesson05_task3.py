02c1bb429a37d8c27ac4c495f276b2983d4e8f5f
ZojaDD <zojadr.zd@gmail.com>
2026-02-04 21:57:07 +0300

    Исправлены ошибки

---
+++ 05_lesson/lesson05_task3.py
@@ -0,0 +1,28 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+
+# Перейти на страницу
+driver.get("http://the-internet.herokuapp.com/inputs")
+
+# Ввести в поле текст Sky
+
+#input_field = driver.find_element(By.CSS_SELECTOR, "#content > div > div > div > input[type=number]")
+input_field = driver.find_element(By.CSS_SELECTOR, "input")
+input_field.send_keys("200") 
+
+sleep(5)
+
+# очистить поле
+input_field.clear()
+
+# Ввести в поле текст Pro
+input_field.send_keys("1000") 
+
+sleep(5)
+
+# Закрыть браузер
+driver.quit()
