926a320bd57a16342b180676c4c7b11fdf547a8b
ZojaDD <zojadr.zd@gmail.com>
2026-02-04 22:20:09 +0300

    Исправлен lesson05_task3.ru

--- 05_lesson/lesson05_task3.py
+++ 05_lesson/lesson05_task3.py
@@ -9,7 +9,12 @@ driver.get("http://the-internet.herokuapp.com/inputs")
 
 # Ввести в поле текст Sky
 input_field = driver.find_element(By.CSS_SELECTOR, "input")
-input_field.send_keys("200")
+
+# Меняем тип поля на 'text'
+driver.execute_script("arguments[0].setAttribute('type', 'text');",
+                      input_field)
+
+input_field.send_keys("Sky")
 
 sleep(5)
 
@@ -17,7 +22,7 @@ sleep(5)
 input_field.clear()
 
 # Ввести в поле текст Pro
-input_field.send_keys("1000")
+input_field.send_keys("Pro")
 
 sleep(5)
 
