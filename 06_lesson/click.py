75051b4557aef71aa10d920420dd8c5de3ff975a
ZojaDD <zojadr.zd@gmail.com>
2026-02-11 17:26:55 +0300

    Создана ветка lesson6

---
+++ 06_lesson/click.py
@@ -0,0 +1,5 @@
+#Перейдите на страницу http://uitestingplayground.com/ajax.
+# Нажмите на синюю кнопку.
+# Получите текст из зеленой плашки.
+# Выведите его в консоль 
+# ("Data loaded with AJAX get request.")
