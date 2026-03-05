63ffc3bd640cc2938d987d65ffe3a5f4b45df18b
ZojaDD <zojadr.zd@gmail.com>
2026-03-05 15:48:51 +0300

    Добавлен CalculatorPage.py

--- 07_lesson/pages/CalculatorPage.py
+++ 07_lesson/pages/CalculatorPage.py
@@ -6,3 +6,24 @@ class CalculatorPage:
     def __init__(self, driver):
         self._driver = driver
         self.wait = WebDriverWait(driver, 5)
+
+    def open_page(self):
+        self._driver.get(
+            'https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html')
+
+
+    def delay_input_field(self):
+        element = self._driver.find_element(By.CSS_SELECTOR, "#delay")
+        element.clear()
+        element.send_keys("45")
+    
+    def press_buttons(self):
+        buttons = ["7", "+", "8", "="]
+        for button in buttons:
+            xpath = f"//span[text()='{button}']"
+            self._driver.find_element(By.XPATH, xpath).click()
+
+    def check_result(self):
+        result = WebDriverWait(self._driver, 46).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
+        )
