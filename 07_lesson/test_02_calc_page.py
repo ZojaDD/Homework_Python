f89c3a634aa24325cc4773861a9fca46807b7a47
ZojaDD <zojadr.zd@gmail.com>
2026-03-05 15:48:58 +0300

    modified:   07_lesson/test_02_calc_page.py

--- 07_lesson/test_02_calc_page.py
+++ 07_lesson/test_02_calc_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from pages.CalculatorPage import CalculatorPage
 
 @pytest.fixture
 def driver():
@@ -18,22 +18,30 @@ def driver():
 
 
 def test_calculator(driver):
-    driver.get(
-        'https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html')
+    calc_page = CalculatorPage(driver)
+    open_page(self)
+    delay_input_field(self)
+    press_buttons(self)
+    check_result(self)
 
-    # В поле ввода по локатору #delay введите значение 45.
-    element = driver.find_element(By.CSS_SELECTOR, "#delay")
-    element.clear()
-    element.send_keys("45")
 
-    # Нажмите на кнопки: "7", "+", "8", "="
-    buttons = ["7", "+", "8", "="]
-    for button in buttons:
-        xpath = f"//span[text()='{button}']"
-        driver.find_element(By.XPATH, xpath).click()
+# def test_calculator(driver):
+#     driver.get(
+#         'https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html')
+
+#     # В поле ввода по локатору #delay введите значение 45.
+#     element = driver.find_element(By.CSS_SELECTOR, "#delay")
+#     element.clear()
+#     element.send_keys("45")
+
+    # # Нажмите на кнопки: "7", "+", "8", "="
+    # buttons = ["7", "+", "8", "="]
+    # for button in buttons:
+    #     xpath = f"//span[text()='{button}']"
+    #     driver.find_element(By.XPATH, xpath).click()
 
     # Проверьте (assert), что в окне отобразится результат 15 через 45 секунд.
-    result = WebDriverWait(driver, 46).until(
-        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
-    )
+    # result = WebDriverWait(driver, 46).until(
+    #     EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
+    # )
     assert result
