15d8d593e5c9651f9506a7384461b52a8098e213
ZojaDD <zojadr.zd@gmail.com>
2026-03-14 09:55:46 +0300

    Внесены изменения Добавлена Инструкция.txt

--- 08_lesson/test_project_api.py
+++ 08_lesson/test_project_api.py
@@ -1,24 +1,18 @@
-from dotenv import load_dotenv
-import os
 import requests
 
-# Загрузить переменные из .env
-load_dotenv()
-
 # Получить значения переменных
-url = os.getenv('base_url')
-login = os.getenv('login')
-password = os.getenv('password')
-company_id = os.getenv('company_id')
-API_key = os.getenv('API_key')
-token = "SAxeR+W3BkrLoNjuLgPSeW73lkEhgMpLmIujOIPcZGAX7KKM0djsb3LcPSnODy47"
+url = "https://ru.yougile.com"
+company_id = "f0fde861-acc8-479e-9a00-b0ddbc0a6502"
+login = # ваш_логин_здесь
+password = # ваш_пароль
+token = # ваш_токен_здесь
+user_id = # ваш_user_id_здесь
 
 # Указать URL и параметры запроса
-url = "https://ru.yougile.com"
 params = {
     'login': login,
     'password': password,
-    'companyID': company_id
+    'companyId': company_id
 }
 
 my_headers = {
@@ -27,75 +21,124 @@ my_headers = {
 }
   #Авторизация
 def test_auth():
-    response = requests.post(url + '/api-v2/auth/keys', json=params)
-    print(url + '/api-v2/auth/keys')
-    print(params)
+    response = requests.post(url + '/api-v2/auth/keys/get', json=params)
     response_body = response.json()
     print(response_body)
-    assert response.status_code == 200
-
-# Получить список проектов
-def test_list_projects():
-    response = requests.get(url + "/api-v2/projects", headers=my_headers)
-    response_body = response.json()
-    print(response_body)
-    first_company = response_body['content']
-    print(first_company)
-
-# Получить количество проектов
-def test_list_projects():
-    response = requests.get(url + "/api-v2/projects", headers=my_headers)
-    response_body = response.json()
-    len_before = len(response_body)
-    print(response_body)
-    print(len_before)
-
-    assert len(response_body) > 0
     assert response.status_code == 200
 
 def test_add_new():
-    # Добавить проект
+    # Получить список проектов
     response = requests.get(url + "/api-v2/projects", headers=my_headers)
     response_body = response.json()
-    len_before = len(response_body)
-
+    # Считаем количество проектов
+    company_list = response_body['content']
+    len_before = len(company_list)
+    # Проверяем, что список проектов не пустой
+    assert len(company_list) > 0
+    assert response.status_code == 200
+    # Добавляем новый проект
     project = {
         "title": "Домашка 8",
         "users": {
-            'b1e0e27d-d59d-483c-8c71-06681973fc95': "admin"
+            user_id : "admin"
         }
     }
     response = requests.post(url + '/api-v2/projects', json=project, headers=my_headers)
-
-    # Получить количество проектов после
+    assert response.status_code == 201, f'пришел код ответа {response.status_code}'
+    # Получить количество проектов после добавления
+    response = requests.get(url + "/api-v2/projects", headers=my_headers)
     response_body = response.json()
-    len_after = len(response_body)
+    # Считаем количество проектов
+    company_list = response_body['content']
+    len_after = len(company_list)
     print(len_after)
-
     # Проверить, что +1
     assert len_after - len_before == 1
 
+def test_add_negative():
+    # Получить список проектов
+    response = requests.get(url + "/api-v2/projects", headers=my_headers)
+    response_body = response.json()
+    # Считаем количество проектов
+    company_list = response_body['content']
+    len_before = len(company_list)
+    # Пытаемся создать новый проект без названия
+    project = {
+        "title": "",
+        "users": {
+            user_id : "admin"
+        }
+    }
+    response = requests.post(url + '/api-v2/projects', json=project, headers=my_headers)
+    assert response.status_code == 400, f'пришел код ответа {response.status_code}'
+    # Получить количество проектов после добавления
+    response = requests.get(url + "/api-v2/projects", headers=my_headers)
+    response_body = response.json()
+    # Считаем количество проектов
+    company_list = response_body['content']
+    len_after = len(company_list)
+    # Проверить, что количество проектов не изменилось
+    assert len_after - len_before == 0
 
 def test_change_project():
     # Создать новый проект
     project = {
         "title": "Новая домашка 8",
         "users": {
-            "b1e0e27d-d59d-483c-8c71-06681973fc95": "admin"
+            user_id : "admin"
         }
     }
     response = requests.post(url + '/api-v2/projects', json=project, headers=my_headers)
-    response_body = response.json()
     # Получить id нового проекта
     project_id = response.json().get("id")
-    print(project_id)
-
-    # Изменить проект
-    {
-        "title": "Новый проект для домашки8"
+    # Изменить название проекта
+    project = {
+        "title": "Изменить проект"
     }
-    response = requests.post(url + /api-v2/projects/{project_id}, json=project, headers=my_headers)
-    response_body = response.json()
+    response = requests.put(url + f"/api-v2/projects/{project_id}", json=project, headers=my_headers)
+    project_id = response.json().get("id")
+    # получить измененный проект по id
+    response = requests.get(url + f"/api-v2/projects/{project_id}", headers=my_headers)
+    name = response.json().get("title")
+    assert response.status_code == 200
+    # Проверка что название изменилось
+    assert name == "Изменить проект"
+
+def test_change_negative():
+    # Создать новый проект
+    project = {
+        "title": "Негатив",
+        "users": {user_id: "admin"
+        }
+    }
+    response = requests.post(url + '/api-v2/projects', json=project, headers=my_headers)
+    # Получить id нового проекта
+    project_id = response.json().get("id")
+    # Изменить название проекта
+    project = {
+        "title": ""
+    }
+    response = requests.put(url + f"/api-v2/projects/{project_id}", json=project, headers=my_headers)
+    project_id = response.json().get("id")
+    # получить измененный проект по id
+    response = requests.get(url + f"/api-v2/projects/{project_id}", headers=my_headers)
+    name = response.json().get("title")
+    assert response.status_code == 404, f'пришел код ответа {response.status_code}'
+    # Проверка что название не изменилось
+    assert name == None
 
 
-
+def test_get_id_negative():
+    # Создать новый проект
+    project = {
+        "title": "Негатив",
+        "users": {user_id: "admin"
+                  }
+    }
+    response = requests.post(url + '/api-v2/projects', json=project, headers=my_headers)
+    # Получить id нового проекта
+    project_id = response.json().get("id")
+    # Попытаемся получить проект по неправильному id
+    response = requests.get(url + f"/api-v2/projects/{project_id}000", headers=my_headers)
+    name = response.json().get("title")
+    assert response.status_code == 404, f'пришел код ответа {response.status_code}'
